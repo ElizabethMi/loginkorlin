4b852518c5ea3e53b2dfe634c2184a08cbdfbfe3
elizabeth <213641285eli@gmail.com>
2019-09-22 15:52:09 -0500

    Initial commit

---
+++ app/src/main/java/mir/gro/loginkorlin/activity_cuarta.kt
@@ -0,0 +1,22 @@
+package mir.gro.loginkorlin
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_cuarta.*
+
+class activity_cuarta : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuarta)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
